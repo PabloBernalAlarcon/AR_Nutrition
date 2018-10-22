fd79d5b0d3b9b003b21fb8a196cb7444031f4247
PabloBernalAlarcon <pbernalalarcon@fullsail.edu>
2018-10-22 16:17:33 -0400

    the cube faces are updated every frame, preventing the weird positioning issue

--- Vuforia Project/Assets/Scripts/BuildableCube.cs
+++ Vuforia Project/Assets/Scripts/BuildableCube.cs
@@ -6,33 +6,43 @@ public class BuildableCube : MonoBehaviour {
 
     public Vector3[] Faces;
 
+    public bool Destructible;
   
 	// Use this for initialization
 	void Start () {
-        Faces = new Vector3[6];
+        if (Destructible)
+            Faces = new Vector3[6];
+        else
+            Faces = new Vector3[1];
+
         updateFaces();
 	}
 
     void updateFaces()
     {
+        if (Destructible)
+        {
+            //z axis
+            Faces[0] = transform.forward;
+            Faces[1] = -Faces[0];
 
-        //z axis
-        Faces[0] = transform.forward;
-        Faces[1] = -Faces[0];
+            //y axis
+            Faces[2] = transform.up;
+            Faces[3] = -Faces[2];
 
-        //y axis
-        Faces[2] = transform.up;
-        Faces[3] = -Faces[2];
-
-        //x axis
-        Faces[4] = transform.right;
-        Faces[5] = -Faces[4];
+            //x axis
+            Faces[4] = transform.right;
+            Faces[5] = -Faces[4];
+        }
+        else
+            Faces[0] = transform.up;
     }
 
-   /* private void Update()
+
+    private void Update()
     {
         updateFaces();
 
-        transform.Rotate(0,Time.deltaTime* 3, 0);
-    }*/
+       // transform.Rotate(0, Time.deltaTime * 3, 0);
+    }
 }
--- Vuforia Project/Assets/Scripts/CubeFaceHighlighter.cs
+++ Vuforia Project/Assets/Scripts/CubeFaceHighlighter.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class CubeFaceHighlighter : MonoBehaviour {
     [SerializeField]
    GameObject FaceBall;
-
+    [SerializeField]
+    GameObject CubeToSpawn;
+    [SerializeField]
+    Transform daddy;
     Vector3 Offset = Vector3.zero;
     Camera camera;
+
+    [SerializeField]
+    bool DebbugingOnEditor;
 	// Use this for initialization
 	void Start () {
         camera = GetComponent<Camera>();
@@ -18,23 +24,30 @@ public class CubeFaceHighlighter : MonoBehaviour {
        // if (Input.GetKeyDown(KeyCode.Mouse0))
        // {
             RaycastHit hit;
-           // Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-        Ray ray = camera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        Ray ray;
+        if (DebbugingOnEditor)
+             ray = camera.ScreenPointToRay(Input.mousePosition);
+        else
+             ray = camera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+
+        
             if (Physics.Raycast(ray, out hit))
             {
                 BuildableCube MyB = hit.transform.GetComponent<BuildableCube>();
                 if (MyB)
                 {
-                    GameObject temp;
-                        Debug.DrawLine(hit.point, hit.transform.position, Color.red);
-                        FaceBall.transform.position = CompareVectors(hit.point,hit.transform.position, MyB);
-
+                //    GameObject temp;
+                //        Debug.DrawLine(hit.point, hit.transform.position, Color.red);
+                //FaceBall.transform.position = hit.point;
+                    FaceBall.transform.position = CompareVectors(hit.point,hit.transform, MyB);
                     if (Input.GetKeyDown(KeyCode.Mouse0))
                     {
-                        temp = Instantiate(hit.transform.gameObject,null);
-                        temp.transform.position = hit.transform.position + Offset;
+                    CreateCube(hit.point, hit.transform, daddy, MyB);
+                    //       temp = Instantiate(CubeToSpawn/*hit.transform.gameObject*/,daddy);
+                    //       temp.transform.position = hit.transform.position + Offset;
+
                     }
-                    else if (Input.GetKeyDown(KeyCode.Mouse1))
+                    else if (Input.GetKeyDown(KeyCode.Mouse1) && MyB.Destructible)
                     {
                     Destroy(hit.transform.gameObject);
                     }
@@ -43,16 +56,16 @@ public class CubeFaceHighlighter : MonoBehaviour {
             else
             {
             FaceBall.transform.position = new Vector3(9999999, 9999999, 99999);
-                print("I see no face");
+              //  print("I see no face");
             }
 
         
        // }
     }
 
-    Vector3 CompareVectors(Vector3 item, Vector3 Position,BuildableCube toCompare)
+    Vector3 CompareVectors(Vector3 item, Transform Position,BuildableCube toCompare)
     {
-        Vector3 deltaPos = item - Position;
+        Vector3 deltaPos = item - Position.position;
         float res = -90;
         Vector3 closest = Vector3.zero;
         int index = 0;
@@ -67,10 +80,38 @@ public class CubeFaceHighlighter : MonoBehaviour {
                 index = i;
             }
         }
+       // for (int i = 0; i < 3; i++)
+       // {
+       //     closest[i] *= Position.localScale[i];
+       // }
             Offset = closest;
-            return Position + (closest / 2);          
+            return Position.position + (closest / 2);          
     }
 
+    void CreateCube(Vector3 item, Transform Position, Transform _daddy, BuildableCube toCompare){
+        Vector3 deltaPos = item - Position.position;
+        float res = -90;
+        Vector3 closest = Vector3.zero;
+        int index = 0;
+        for (int i = 0; i < toCompare.Faces.Length; i++)
+        {
+
+            float value = Vector3.Dot(deltaPos.normalized, toCompare.Faces[i].normalized);
+            if (value >= res)
+            {
+                res = value;
+                closest = toCompare.Faces[i];
+                index = i;
+            }
+        }
+      
+        Offset = closest;
+
+        FaceBall.transform.position = Position.position + (closest / 2);
+        GameObject temp = Instantiate(CubeToSpawn, (Position.position + Offset),CubeToSpawn.transform.rotation,_daddy);
+        //temp.transform.position = Position.position + Offset;
+        
+    }
 
 
    
