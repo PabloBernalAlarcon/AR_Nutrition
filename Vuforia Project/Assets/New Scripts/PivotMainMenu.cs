dcec74f899f4e23d43c6b3cac55e879b3afea434
PabloBernalAlarcon <pbernalalarcon@fullsail.edu>
2018-12-28 16:34:40 -0500

    There's a new menu, bois and gals!
    The new menu has music, sound effects, animations and a cool font effect
    for the title. it looks pretty nice. Plus, the code is on point so far,
    I hope.

---
+++ Vuforia Project/Assets/New Scripts/PivotMainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PivotMainMenu : MonoBehaviour {
+
+    [SerializeField]
+    bool RandomNumber;
+    [SerializeField]
+    float RotationSpeed;
+
+    private void Start()
+    {
+        if (RandomNumber)
+            RotationSpeed = Random.Range(-5f,5f);
+    }
+    // Update is called once per frame
+    void Update () {
+        transform.Rotate(0, 0, Time.deltaTime*RotationSpeed);
+	}
+}
