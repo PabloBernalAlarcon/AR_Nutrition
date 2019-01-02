1b161c361f811cc39b6417b95fde0e3a05187619
PabloBernalAlarcon <pbernalalarcon@fullsail.edu>
2019-01-02 14:48:15 -0500

    There is text surrounding the rocket now.

---
+++ Vuforia Project/Assets/WorldUIBubble.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldUIBubble : MonoBehaviour {
+
+    [SerializeField]
+    Transform cam;
+
+	
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 targetPostition = new Vector3(-cam.position.x,
+                                       this.transform.position.y,
+                                       -cam.position.z);
+        this.transform.LookAt(targetPostition);
+	}
+}
