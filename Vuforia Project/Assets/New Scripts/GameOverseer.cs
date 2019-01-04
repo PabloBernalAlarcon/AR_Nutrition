1a021f322116f6e7669470fab3bfff204c6283b5
PabloBernalAlarcon <pbernalalarcon@fullsail.edu>
2019-01-04 12:38:23 -0500

    World space UI is now clickable and more
    There's a '?' button now
    UI elements dissappear when the image target i lost.

--- Vuforia Project/Assets/New Scripts/GameOverseer.cs
+++ Vuforia Project/Assets/New Scripts/GameOverseer.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public  class GameOverseer :MonoBehaviour {
 
+    //*****PUBLIC MEMBERS*****
+
     //Singleton instence for the class
     public static GameOverseer instance;
-
+    //delegate related to image targeting
     public delegate void AREvent(bool _trigger);
-
+    //event related to when the image target was found or lost
     public  event AREvent ARTargetFoundStatus;
+
+    //*****PRIVATE MEMBERS*****
+
+    //List of elements affected by tracking
+    private List<GameObject> UIElements;
+
+    //*****METHODS*****
 
     private void Awake()
     {
+        
         //Do I exist?
         if (instance == null)
         {
@@ -23,22 +33,54 @@ public  class GameOverseer :MonoBehaviour {
             Destroy(this.gameObject);
         }
         DontDestroyOnLoad(this);
+
+        UIElements = new List<GameObject>();
     }
 
+    //Changes the status (active/not) of the UIElement's items
+    private void ChangeUIElementsStatus(bool _Active)
+    {
+        foreach (GameObject item in UIElements)
+            item.SetActive(_Active);       
+    }
+
+    //found Image Target
     public void Found()
     {
-        //base.OnTrackingFound();
+        Time.timeScale = 1;
+
         print("fond ye");
+
+        ChangeUIElementsStatus(true);
 
         if(ARTargetFoundStatus!= null)
             ARTargetFoundStatus(true);
     }
 
+    //Lost Image Target
     public void Lost()
     {
-       // base.OnTrackingLost();
+        Time.timeScale = 0;
+
         print("You lost me babe");
+
+        ChangeUIElementsStatus(false);
+
         if(ARTargetFoundStatus != null)
             ARTargetFoundStatus(false);
     }
+
+    //Clear List elements -- use every time
+    //the scene is changed since this is script is not deleted.
+    public void ResetList()
+    {
+        UIElements.Clear();
+    }
+
+    //Adds a element to the list. to be called from other scripts on awake awake.
+    public void AddUIItem(GameObject UIitem)
+    {
+        UIElements.Add(UIitem);
+    }
+
 }
