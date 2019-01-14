c97b2ab3cafc701cfa94f12afdc73364da458054
PabloBernalAlarcon <pbernalalarcon@fullsail.edu>
2019-01-14 10:23:24 -0500

    Particles animate on liftoff

---
+++ Vuforia Project/Assets/ParticleEdit.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleEdit : MonoBehaviour {
+
+    ParticleSystem PS;
+	// Use this for initialization
+	void Start () {
+        PS = GetComponent<ParticleSystem>();
+	}
+
+    private void OnEnable()
+    {
+        UberRocket.fadeOut += ChangeParticleAngle;
+    }
+    private void OnDisable()
+    {
+        UberRocket.fadeOut -= ChangeParticleAngle;
+    }
+
+    void ChangeParticleAngle()
+    {
+        StartCoroutine(ChangeParticleAngleCor());
+    }
+
+	IEnumerator ChangeParticleAngleCor()
+    {
+        var shkere = PS.shape;
+
+        for (float i = 90; i >= 0; i-= (Time.deltaTime *10))
+        {
+            shkere.angle = i;
+        yield return null;
+        }
+    }
+}
