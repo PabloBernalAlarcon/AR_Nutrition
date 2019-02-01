dbb76904a6d0bcab3c67260d9c67d1de12d589af
PabloBernalAlarcon <pbernalalarcon@fullsail.edu>
2019-01-31 19:24:19 -0500

    Started screenshotting
    Added the native screenshot plugin
    Added another canvas to the scene

---
+++ Vuforia Project/Assets/New Scripts/TakePhotoAndStore.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakePhotoAndStore : MonoBehaviour {
+
+    void Start()
+    {
+        Debug.Log(Application.persistentDataPath);
+    }
+    public void TakePhoto()
+    {
+      
+    }
+
+}
