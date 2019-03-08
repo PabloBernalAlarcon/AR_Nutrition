62c5697756e81995ecb43729aecbc37ae814e749
PabloBernalAlarcon <pbernalalarcon@fullsail.edu>
2019-03-07 19:30:11 -0500

    pailas totales
    paila

--- Vuforia Project/Assets/Scripts/CameraFocusController.cs
+++ Vuforia Project/Assets/Scripts/CameraFocusController.cs
@@ -3,36 +3,36 @@
 
 using UnityEngine;
 using System.Collections;
-using Vuforia;
+//using Vuforia;
 
 public class CameraFocusController : MonoBehaviour
 {
-    Camera c;
-    // code from  Vuforia Developer Library
-    // https://library.vuforia.com/articles/Solution/Camera-Focus-Modes
-    void Start()
-    {
-        c = GetComponent<Camera>();
-        var vuforia = VuforiaARController.Instance;
-        vuforia.RegisterVuforiaStartedCallback(OnVuforiaStarted);
-        vuforia.RegisterOnPauseCallback(OnPaused);
-    }
+    //Camera c;
+    //// code from  Vuforia Developer Library
+    //// https://library.vuforia.com/articles/Solution/Camera-Focus-Modes
+    //void Start()
+    //{
+    //    c = GetComponent<Camera>();
+    //   // var vuforia = VuforiaARController.Instance;
+    //   // vuforia.RegisterVuforiaStartedCallback(OnVuforiaStarted);
+    //   // vuforia.RegisterOnPauseCallback(OnPaused);
+    //}
 
-    private void OnVuforiaStarted()
-    {
-        CameraDevice.Instance.SetFocusMode(
-            CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
-    }
+    //private void OnVuforiaStarted()
+    //{
+    //    CameraDevice.Instance.SetFocusMode(
+    //        CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+    //}
 
-    private void OnPaused(bool paused)
-    {
-        if (!paused) // resumed
-        {
-            // Set again autofocus mode when app is resumed
-            CameraDevice.Instance.SetFocusMode(
-               CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
-        }
-    }
+    //private void OnPaused(bool paused)
+    //{
+    //    if (!paused) // resumed
+    //    {
+    //        // Set again autofocus mode when app is resumed
+    //        CameraDevice.Instance.SetFocusMode(
+    //           CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+    //    }
+    //}
 
  
 }
